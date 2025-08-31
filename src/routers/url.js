02a73deb8d3a1aa332b558af8d6e86182867a9df
shubhamjeurkar <shubhamjeurkar11@gmail.com>
2025-08-31 20:23:14 +0530

    fix: update import paths for URL controller to match file naming convention

--- src/routers/url.js
+++ src/routers/url.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { createShortURL, getShortURL, redirectToLongURL, deleteShortURL, getSingleShortURL } = require('../controllers/url');
+const { createShortURL, getShortURL, deleteShortURL, getSingleShortURL } = require('../controllers/url');
 
 router.post("/urls", createShortURL).get("/urls", getShortURL);
 router.get("/urls/:id", getSingleShortURL).delete("/urls/:id", deleteShortURL);
